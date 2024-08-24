b2971f5b85e88d1ef8adbde13bd31787bde8996b
NathanJohnson <nathanj@stratusadv.com>
2024-08-23 20:32:44 -0600

    Work flow!

--- dandy/workflow/workflow.py
+++ dandy/workflow/workflow.py
@@ -2,14 +2,12 @@ from typing import List, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from dandy.workflow.step import Step
+    from dandy.job.job import Job
 
 
 class Workflow:
     steps: List[Step]
 
-    def __init__(self):
-        pass
-
-    def process(self, job: 'Job'):
+    def process(self, job: Job):
        for step in self.steps:
             step.process(job)
