7efe4026d3f3f01138e838a72ebb2a54fccc1da0
NathanJohnson <nathanj@stratusadv.com>
2024-09-21 00:35:51 -0600

    Debug tracking pydantic error

--- dandy/llm/service/prompts.py
+++ dandy/llm/service/prompts.py
@@ -33,12 +33,9 @@ def service_system_model_prompt(
 def service_system_validation_error_prompt(e: ValidationError) -> Prompt:
     # TODO: use pydantic's error mapping is not the greatest ... should be improved
     ve = json.loads(e.json())
-    print(ve)
     ve_string = ''
     for error in ve:
-        ve_string += f'{error["type"].__str__()}: {error["input"].__str__()}\n'.replace("'", '"')
-
-    print(ve_string)
+        ve_string += f'{error["type"].__str__()}: {error["loc"].__str__()} {error["input"].__str__()}\n'.replace("'", '"')
 
     return (
         Prompt()
