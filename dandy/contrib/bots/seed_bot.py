60b536970307e4acd36f60801dbaaf83c9049daf
NathanJohnson <nathanj@stratusadv.com>
2024-09-30 16:40:56 -0600

    Clean out seed branch

--- dandy/contrib/bots/seed_bot.py
+++ dandy/contrib/bots/seed_bot.py
@@ -4,20 +4,11 @@ from pydantic import BaseModel
 from typing import Any, Type
 
 from dandy.bot import LlmBot
-from dandy.debug.debug import DebugRecorder
 from dandy.llm.config.ollama import OllamaLlmConfig
 from dandy.llm.prompt import Prompt
-from tests.models.tradesman_model import GroupBase
 
 
 class SeedLlmBot(LlmBot):
-    llm_config = OllamaLlmConfig(
-        host=os.getenv('OLLAMA_HOST'),
-        port=int(os.getenv('OLLAMA_PORT')),
-        model='llama3.1',
-        temperature=1.0,
-    )
-
     role_prompt = (
         Prompt()
         .text('You are a data generation bot tasked with generating realistic data for Pydantic models.')
@@ -41,21 +32,8 @@ class SeedLlmBot(LlmBot):
         model_instance = cls.process_prompt_to_model_object(
             prompt=prompt,
             model=model,
-            **kwargs
         )
 
         return model_instance
 
 
-def main() -> None:
-    DebugRecorder.start_recording('playground')
-
-    work_order_instance = SeedLlmBot.process(GroupBase)
-    print(work_order_instance.json())
-
-    DebugRecorder.stop_recording('playground')
-    DebugRecorder.to_html('playground')
-
-
-if __name__ == '__main__':
-    main()
