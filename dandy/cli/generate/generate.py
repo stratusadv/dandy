5d8aaabb6ce3dcef618dcd11cbf5500d853fd3bb
NathanJohnson <nathanj@stratusadv.com>
2024-11-16 22:50:47 -0700

    update generation to show only llmbot

--- dandy/cli/generate/generate.py
+++ dandy/cli/generate/generate.py
@@ -7,7 +7,7 @@ from dandy.cli.generate.intelligence.prompts.generate_llm_bot_prompts import gen
     generate_llm_bot_user_prompt
 
 
-GENERATE_CHOICES = ['workflow', 'llmbot', 'bot']
+GENERATE_CHOICES = ['llmbot']
 
 class LlmBotSource(BaseModel):
     file_name: str
@@ -22,31 +22,15 @@ def generate(choice: str, description: Union[str, None] = None):
 
     print(f'Generating {choice} ... depending on your llm configuration this may take up to a couple minutes')
 
-    if choice == 'workflow':
-        print('Generating Workflow')
-
-    elif choice == 'llmbot':
-        # print(generate_llm_bot_system_prompt().to_str())
-        # print('------------------')
-
+    if choice == 'llmbot':
         llm_bot_source = settings.DEFAULT_LLM_CONFIG.service.process_prompt_to_model_object(
             prompt=generate_llm_bot_user_prompt(user_input),
             model=LlmBotSource,
             prefix_system_prompt=generate_llm_bot_system_prompt(),
         )
 
-        # print('------------------')
-        # print('Generating LlmBot')
-        # print('------------------')
-        # print(llm_bot_source.file_name)
-        # print('------------------')
-        # print(llm_bot_source.source)
-
         with open(llm_bot_source.file_name, 'w') as f:
             f.write(llm_bot_source.source)
 
         print(f'Done ... saved to "{settings.CURRENT_PATH / llm_bot_source.file_name}"')
 
-
-    elif choice == 'bot':
-        print('Generating Bot')
