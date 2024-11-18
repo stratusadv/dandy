e2ff8e721ce8195411af88d739dedfaa98381da1
NathanJohnson <nathanj@stratusadv.com>
2024-11-17 18:48:32 -0700

    updated run scripts for the dev env

--- .dev/run_tests.py
+++ .dev/run_tests.py
@@ -6,22 +6,7 @@ from pathlib import Path
 
 from utils import load_environment_variables
 
-OMITS = (
-    # Directories
-    '*/venv/*',
-    '*/.venv/*',
-
-    # Files
-    '__init__.py',
-    'setup.py',
-    'run_coverage.py'
-)
-
-
 def main() -> None:
-    # Do not ever rename this file to coverage.py as it creates an infinite
-    # loop. This script must run from the root of your project.
-
     if sys.platform != 'win32':
         message = 'This script must run on Windows platform.'
         raise Exception(message)
@@ -35,7 +20,7 @@ def main() -> None:
     parser = argparse.ArgumentParser(description='Run coverage with tests.')
 
     apps_help = (
-        'The specific app(s) to run coverage on. '
+        'The specific app(s) to run testing on. '
         'The path should be relative to the project root. '
         'e.g., app/maintenance/work_order '
         'You can use a space-delimited list to include multiple apps '
@@ -62,34 +47,6 @@ def main() -> None:
         help=venv_help
     )
 
-    no_browser_help = (
-        'Do not open the browser window to show the coverage results.'
-    )
-
-    parser.add_argument(
-        '-nb', '--nobrowser',
-        action='store_true',
-        help=no_browser_help
-    )
-
-    no_erase_help = (
-        'Do not erase the coverage results.'
-    )
-
-    parser.add_argument(
-        '-ne', '--noerase',
-        action='store_true',
-        help=no_erase_help
-    )
-
-    no_html_help = 'Do not write the coverage results to HTML.'
-
-    parser.add_argument(
-        '-nh', '--nohtml',
-        action='store_true',
-        help=no_html_help
-    )
-
     args = parser.parse_args()
 
     if Path(args.venv).is_file():
@@ -106,45 +63,26 @@ def main() -> None:
 
     pip_install_coverage_cmd = 'pip install coverage'
 
-    print('Running coverage with tests ...\n')
+    print('Running Testing ...\n')
 
-    omits = ','.join(OMITS)
-
-    coverage_run_cmd = [
-        f'coverage',
-        'run',
-        '--branch',
-        f'--source={cwd}',
-        f'--omit={omits}',
+    run_testing_cmd = [
+        'python',
         '-m',
         'unittest',
         'discover',
+        '-v'
     ]
 
-    coverage_run_cmd = ' '.join(coverage_run_cmd)
+    run_testing_cmd = ' '.join(run_testing_cmd)
 
-    print(coverage_run_cmd)
+    print(run_testing_cmd)
 
     cmd_call = (
         f'call {activate_virtualenv_cmd}'
         f' & {pip_install_coverage_cmd}'
         f' & cd..'
-        f' & {coverage_run_cmd}'
+        f' & {run_testing_cmd}'
     )
-
-    html_directory = '.coverage_html_report'
-
-    if not args.nohtml:
-        coverage_html_cmd = f'coverage html --directory={html_directory}'
-        cmd_call += f' & {coverage_html_cmd}'
-
-    if not args.noerase:
-        coverage_erase_cmd = f'coverage erase'
-        cmd_call += f' & {coverage_erase_cmd}'
-
-    if not args.nobrowser:
-        open_browser_cmd = f'start "" "{cwd / html_directory / "index.html"}"'
-        cmd_call += f' & {open_browser_cmd}'
 
     subprocess.run(cmd_call, check=True, shell=True)
 
