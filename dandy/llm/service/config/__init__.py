22658d3abc1205145812429a1c0e63690ba963b8
Nathan Johnson <nathanj@stratusadv.com>
2025-03-04 22:58:22 -0700

    Fix import error

--- dandy/llm/service/config/__init__.py
+++ dandy/llm/service/config/__init__.py
@@ -1,3 +1,4 @@
 from dandy.llm.service.config.config import BaseLlmConfig
 from dandy.llm.service.config.ollama import OllamaLlmConfig
 from dandy.llm.service.config.openai import OpenaiLlmConfig
+from dandy.llm.service.config.options import LlmConfigOptions
