5233a1e2fe5c722d731225522bb65114f7b7ba68
Nathan Johnson <nathanj@stratusadv.com>
2025-04-27 19:27:25 -0600

    remove llm bot vision test

--- tests/llm/bot/test_llm_bot_vision.py
+++ tests/llm/bot/test_llm_bot_vision.py
@@ -45,23 +45,24 @@ class ImageBreakdownLlmBot(BaseLlmBot[ImageBreakdownIntel]):
 
 
 class TestLlmBotVision(TestCase):
-    @run_llm_configs(['GEMMA_3_12B_VISION', 'GPT_4o'])
-    def test_llm_bot_intel_class_include(self, llm_config: str):
-        ImageBreakdownLlmBot.config = llm_config
-
-        image_breakdown_intel = ImageBreakdownLlmBot.process(
-            prompt=(
-                Prompt()
-                .text('Please describe the following image and count the objects in the image?')
-                .text('Count all the vehicles even if they are partially visible.')
-                .text('How many buildings are there in the image?')
-                .text('Respond with a description for each person in the image.')
-            ),
-            intel_class=ImageBreakdownIntel,
-            image_files=[
-                Path(settings.BASE_PATH, 'assets', 'images', 'vision_test_image.jpg'),
-            ],
-        )
-
-        self.assertEqual(image_breakdown_intel.elk, 1)
-        self.assertEqual(image_breakdown_intel.buildings, 2)
+    pass
+    # @run_llm_configs(['GEMMA_3_12B_VISION', 'GPT_4o'])
+    # def test_llm_bot_intel_class_include(self, llm_config: str):
+    #     ImageBreakdownLlmBot.config = llm_config
+    #
+    #     image_breakdown_intel = ImageBreakdownLlmBot.process(
+    #         prompt=(
+    #             Prompt()
+    #             .text('Please describe the following image and count the objects in the image?')
+    #             .text('Count all the vehicles even if they are partially visible.')
+    #             .text('How many buildings are there in the image?')
+    #             .text('Respond with a description for each person in the image.')
+    #         ),
+    #         intel_class=ImageBreakdownIntel,
+    #         image_files=[
+    #             Path(settings.BASE_PATH, 'assets', 'images', 'vision_test_image.jpg'),
+    #         ],
+    #     )
+    #
+    #     self.assertEqual(image_breakdown_intel.elk, 1)
+    #     self.assertEqual(image_breakdown_intel.buildings, 2)
