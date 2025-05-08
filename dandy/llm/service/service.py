3d88f8c3ba1472863a4fd0d2caf25953cde4f6d4
Nathan Johnson <nathanj@stratusadv.com>
2025-05-07 19:20:20 -0600

    service refactor

--- dandy/llm/service/service.py
+++ dandy/llm/service/service.py
@@ -14,7 +14,8 @@ from dandy.intel.factory import IntelFactory
 from dandy.intel.type_vars import IntelType
 from dandy.llm.exceptions import LlmCriticalException, LlmValidationCriticalException, LlmRecoverableException
 from dandy.llm.prompt import Prompt
-from dandy.llm.service.recorder import recorder_add_llm_request_event, recorder_add_llm_response_event, recorder_add_llm_success_event, \
+from dandy.llm.service.recorder import recorder_add_llm_request_event, recorder_add_llm_response_event, \
+    recorder_add_llm_success_event, \
     recorder_add_llm_validation_failure_event, recorder_add_llm_retry_event
 from dandy.llm.service.prompts import service_system_validation_error_prompt, service_user_prompt, \
     service_system_prompt
@@ -36,14 +37,16 @@ class LlmService(BaseHttpService):
 
         self._llm_config = llm_config
         self._llm_options = llm_options
-
-    def get_request_body(self) -> BaseRequestBody:
-        return self._llm_config.generate_request_body(
+        self._event_id = generate_new_recorder_event_id()
+        self._intel = None
+        self._intel_json_schema = None
+        self._request_body = self._llm_config.generate_request_body(
             max_input_tokens=self._llm_options.max_input_tokens,
             max_output_tokens=self._llm_options.max_output_tokens,
             seed=self._llm_options.seed,
             temperature=self._llm_options.temperature,
         )
+        self._retry_attempt = 0
 
     def process_prompt_to_intel(
             self,
@@ -63,21 +66,19 @@ class LlmService(BaseHttpService):
         if intel_class is None and intel_object is None:
             raise LlmCriticalException('Must specify either intel_class or intel_object.')
 
-        intel_json_schema = IntelFactory.intel_to_json_inc_ex_schema(
-            intel=intel_class or intel_object,
+        self._intel = intel_class or intel_object
+
+        self._intel_json_schema = IntelFactory.intel_to_json_inc_ex_schema(
+            intel=self._intel,
             include=include_fields,
             exclude=exclude_fields
         )
 
-        event_id = generate_new_recorder_event_id()
-
-        request_body = self.get_request_body()
-
-        request_body.set_format_to_json_schema(
-            intel_json_schema
+        self._request_body.set_format_to_json_schema(
+            self._intel_json_schema
         )
 
-        request_body.add_message(
+        self._request_body.add_message(
             role='system',
             content=service_system_prompt(
                 system_prompt=system_prompt
@@ -86,13 +87,13 @@ class LlmService(BaseHttpService):
 
         if message_history:
             for message in message_history.messages:
-                request_body.add_message(
+                self._request_body.add_message(
                     role=message.role,
                     content=message.content,
                     images=message.images
                 )
 
-        request_body.add_message(
+        self._request_body.add_message(
             role='user',
             content=service_user_prompt(
                 prompt if isinstance(prompt, Prompt) else Prompt(prompt)
@@ -100,42 +101,31 @@ class LlmService(BaseHttpService):
             images=images,
         )
 
-        return self._process_request_to_intel(
-            request_body=request_body,
-            intel_json_schema=intel_json_schema,
-            event_id=event_id,
-            intel=intel_class or intel_object
-        )
-
+        return self._process_request_to_intel()
 
     def _process_request_to_intel(
             self,
-            request_body: BaseRequestBody,
-            intel_json_schema: dict,
-            event_id: str,
-            intel: Union[BaseIntel, Type[BaseIntel]],
-            retry_attempt: int = 0,
     ) -> IntelType:
-        recorder_add_llm_request_event(request_body, intel_json_schema, event_id)
+        recorder_add_llm_request_event(self._request_body, self._intel_json_schema, self._event_id)
 
         message_content = self._llm_config.get_response_content(
             self.post_request(
-                request_body.model_dump()
+                self._request_body.model_dump()
             )
         )
 
-        recorder_add_llm_response_event(message_content, event_id)
+        recorder_add_llm_response_event(message_content, self._event_id)
 
         try:
             intel_object = IntelFactory.json_to_intel_object(
                 message_content,
-                intel
+                self._intel
             )
 
             if intel_object is not None:
                 recorder_add_llm_success_event(
                     'Validated response from prompt into intel object.',
-                    event_id,
+                    self._event_id,
                     intel=intel_object
                 )
 
@@ -144,37 +134,31 @@ class LlmService(BaseHttpService):
             else:
                 raise LlmRecoverableException('Failed to validate response from prompt into intel object.')
 
-        # This validation error would be raised by pydantic if the IntelFactory failed to parse the JSON into an Intel object
         except ValidationError as e:
-            recorder_add_llm_validation_failure_event(e, event_id)
+            recorder_add_llm_validation_failure_event(e, self._event_id)
 
-            if retry_attempt < self._llm_config.options.prompt_retry_count:
+            if self._retry_attempt < self._llm_config.options.prompt_retry_count:
+                self._retry_attempt += 1
+
                 recorder_add_llm_retry_event(
                     'Validation of response to intel object failed retrying with validation errors prompt.',
-                    event_id,
-                    remaining_attempts = self._llm_config.options.prompt_retry_count - (retry_attempt + 1)
+                    self._event_id,
+                    remaining_attempts=self._llm_config.options.prompt_retry_count - (self._retry_attempt + 1)
                 )
 
-                request_body.add_message(
+                self._request_body.add_message(
                     role='assistant',
                     content=message_content
                 )
 
-                request_body.add_message(
+                self._request_body.add_message(
                     role='user',
                     content=service_system_validation_error_prompt(e).to_str()
                 )
 
                 return self._process_request_to_intel(
-                    request_body=request_body,
-                    intel_json_schema=intel_json_schema,
-                    event_id=event_id,
-                    intel=intel,
-                    retry_attempt=retry_attempt + 1
                 )
 
             else:
-                raise LlmRecoverableException(f'Failed to validate response from prompt into intel object after {self._llm_config.options.prompt_retry_count} attempts.')
-
-
-
+                raise LlmRecoverableException(
+                    f'Failed to validate response from prompt into intel object after {self._llm_config.options.prompt_retry_count} attempts.')
