49725b5aa4b65299973cb66c70ff9ec89686ea5d
Nathan Johnson <nathanj@stratusadv.com>
2025-05-12 20:56:49 -0600

    change AgentPlan to use tasks instead of steps

--- dandy/agent/plan/plan.py
+++ dandy/agent/plan/plan.py
@@ -4,4 +4,13 @@ from dandy.agent.plan.task.task import AgentTask
 
 
 class AgentPlan:
-    steps: List[Self, AgentTask]
+    def __init__(self):
+        self.tasks: List[AgentTask] = list()
+        self.active_task_number: int = 0
+
+    def __len__(self) -> int:
+        return len(self.tasks)
+
+    @property
+    def active_task(self) -> AgentTask:
+        return self.tasks[self.active_task_number]
