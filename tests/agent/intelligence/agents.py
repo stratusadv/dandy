a576aada63025e8b0efff158e0dc281c5b1f9a3d
Nathan Johnson <nathanj@stratusadv.com>
2025-09-14 23:14:53 -0600

    update agent test

--- tests/agent/intelligence/agents.py
+++ tests/agent/intelligence/agents.py
@@ -10,7 +10,7 @@ from tests.agent.intelligence.maps import MuseumSubjectMap
 class MuseumEmailAgent(Agent):
     llm_instructions_prompt = (
         Prompt()
-        .text('Write an email to a museum based on the users input, the users email will always be the from address.')
+        .text('Write an email to a museum based on the users request, the users provided email address will always be the from address for the final email.')
         .text('Figure out what subject the user would be most interested and make sure to note that in the email.')
         .text('Confirm the body of the email is well written.')
         .text('Make up an email address to send to based on the museum name.')
