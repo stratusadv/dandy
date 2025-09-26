62abd8f34d247dbcaa7255d21eb8ebf8328c866b
Nathan Johnson <nathanj@stratusadv.com>
2025-09-25 20:19:03 -0600

    settings

--- dandy/default_settings.py
+++ dandy/default_settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 ALLOW_RECORDING_TO_FILE: bool = False
@@ -10,32 +11,31 @@ CACHE_SQLITE_LIMIT: int = 10000
 
 DEBUG: bool = False
 
-# Use 0 to disable the limit on agents think time and thought count
-DEFAULT_AGENT_PLAN_TIME_LIMIT_SECONDS: int = 600 # 10 minutes
-DEFAULT_AGENT_PLAN_TASK_COUNT_LIMIT: int = 100 # This is per agent plan
-
-DEFAULT_LLM_MAX_INPUT_TOKENS: int = 8000
-DEFAULT_LLM_MAX_OUTPUT_TOKENS: int = 4000
-DEFAULT_LLM_PROMPT_RETRY_COUNT: int = 2
-DEFAULT_LLM_RANDOMIZE_SEED: bool = False
-DEFAULT_LLM_REQUEST_TIMEOUT: int | None = None
-DEFAULT_LLM_SEED: int = 77
-DEFAULT_LLM_TEMPERATURE: float = 0.7
+AGENT_DEFAULT_PLAN_TIME_LIMIT_SECONDS: int | None = 600
+AGENT_DEFAULT_PLAN_TASK_COUNT_LIMIT: int | None = 100
 
 HTTP_CONNECTION_RETRY_COUNT: int = 10
-HTTP_CONNECTION_TIMEOUT_SECONDS: int = 120
+HTTP_CONNECTION_TIMEOUT_SECONDS: int | None = None
+
+LLM_DEFAULT_MAX_INPUT_TOKENS: int = 8000
+LLM_DEFAULT_MAX_OUTPUT_TOKENS: int = 4000
+LLM_DEFAULT_PROMPT_RETRY_COUNT: int | None = 2
+LLM_DEFAULT_RANDOMIZE_SEED: bool = False
+LLM_DEFAULT_REQUEST_TIMEOUT: int | None = None
+LLM_DEFAULT_SEED: int = 77
+LLM_DEFAULT_TEMPERATURE: float = 0.7
 
 LLM_CONFIGS = {
-    # 'DEFAULT': {
-    #     'TYPE': 'ollama',
-    #     'HOST': os.getenv("OLLAMA_HOST"),
-    #     'PORT': int(os.getenv("OLLAMA_PORT", 11434)),
-    #     'API_KEY': os.getenv("OLLAMA_API_KEY"),
-    #     'MODEL': 'llama3.1:8b-instruct-q4_K_M',
-    #     'TEMPERATURE': 0.5,
-    #     'SEED': 77,
-    #     'RANDOMIZE_SEED': False,
-    #     'MAX_INPUT_TOKENS': 8000,
-    #     'MAX_OUTPUT_TOKENS': 4000
-    # },
+    'DEFAULT': {
+        'TYPE': 'ollama',
+        'HOST': os.getenv("OLLAMA_HOST"),
+        'PORT': int(os.getenv("OLLAMA_PORT", 11434)),
+        'API_KEY': os.getenv("OLLAMA_API_KEY"),
+        'MODEL': 'a_model:9b',
+        'TEMPERATURE': 0.5,
+        'SEED': 77,
+        'RANDOMIZE_SEED': False,
+        'MAX_INPUT_TOKENS': 8000,
+        'MAX_OUTPUT_TOKENS': 4000
+    },
 }
