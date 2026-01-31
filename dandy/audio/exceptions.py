1ec4b3de93539b9f9cf0fd1ac9a4a8e044886b27
Nathan Johnson <nathanj@stratusadv.com>
2026-01-30 19:52:11 -0700

    remove vision and audio

--- dandy/audio/exceptions.py
+++
@@ -1,9 +0,0 @@
-# from dandy.core.exceptions import DandyCriticalException, DandyRecoverableException
-#
-#
-# class AudioCriticalException(DandyCriticalException):
-#     pass
-#
-#
-# class AudioRecoverableException(DandyRecoverableException):
-#     pass
--- dandy/vision/exceptions.py
+++
@@ -1,9 +0,0 @@
-# from dandy.core.exceptions import DandyCriticalException, DandyRecoverableException
-#
-#
-# class VisionCriticalException(DandyCriticalException):
-#     pass
-#
-#
-# class VisionRecoverableException(DandyRecoverableException):
-#     pass
